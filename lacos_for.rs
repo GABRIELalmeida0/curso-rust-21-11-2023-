1cecec78ef1e2bc3009a6f75324fb5150fc648d3
Wifi IMD <wifi_imd@A3042012017570L.imd.local>
2023-11-21 10:48:37 -0300

    21-11-23

---
+++ lacos_for.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let intervalo = 0..100;
+
+    for celsius in intervalo{
+        let fahrenheit = celsius * 9 / 5 + 32;
+        println!("{celsius}° C = {fahrenheit} F");
+    }
+}
